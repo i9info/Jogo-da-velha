7cf29c3261196415020cda94b84476a1ba43907d
i9info <empresai9info@gmail.com>
2015-05-13 15:20:14 -0300

    commit da primeira parte do codigo, Criação da tabela 3x3

--- src/jogo/Jogo.java
+++ src/jogo/Jogo.java
@@ -1,12 +1,30 @@
 
 package jogo;
 
+import java.util.Scanner;
+
 
 public class Jogo {
 
     
     public static void main(String[] args) {
        
+        Scanner scanner = new Scanner(System.in);
+            int ganhou = 0, escolheJogador = 0;
+            char posicao;
+            char m[][]=new char [3][3];
+            m[0][0]='1';
+            m[0][1]='2';
+            m[0][2]='3';
+            m[1][0]='4';
+            m[1][1]='5';
+            m[1][2]='6';
+            m[2][0]='7';
+            m[2][1]='8';
+            m[2][2]='9';
+            for(int i=0; i<3 ; i++){
+                
+            }
     }
     
 }
