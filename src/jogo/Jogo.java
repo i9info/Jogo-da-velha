234595f20b27b2b995fdd71c63ca4cea621a9e43
i9info <empresai9info@gmail.com>
2015-05-26 09:38:58 -0300

    4 parte

--- src/jogo/Jogo.java
+++ src/jogo/Jogo.java
@@ -10,8 +10,8 @@ public class Jogo {
     public static void main(String[] args) {
        
         Scanner scanner = new Scanner(System.in);
-            int ganhou = 0, escolheJogador = 0;
-            char posicao;
+            int Ganhou = 0, escolheJogador = 0;
+            char Posicao;
             char m[][]=new char [3][3];
             m[0][0]='1';
             m[0][1]='2';
@@ -22,9 +22,9 @@ public class Jogo {
             m[2][0]='7';
             m[2][1]='8';
             m[2][2]='9';
-            for(int i=0; i<3 ; i++){
+            for(int i=0; i<3; i++){
                 
-                 for(int j=0 ; j<3 ; j++){
+                 for(int j=0; j<3; j++){
                        
                         System.out.printf("   "+m[i][j]);
                   }
@@ -37,13 +37,13 @@ public class Jogo {
             if(escolheJogador == 0){    
                  
             System.out.println("Jogador '1', Escolha uma posição");
-            posicao=scanner.nextLine().charAt(0);
+            Posicao=scanner.nextLine().charAt(0);
             
-            for (int i=0 ; i<3 ; i++){ 
-                for(int j=0 ; j<3 ; j++){
+            for (int i=0; i<3; i++){ 
+                for(int j=0; j<3; j++){
             
-                if(posicao==m[i][j]){
-                    m[i][j]='x' ;
+                if(Posicao==m[i][j]){
+                    m[i][j]='x';
                     escolheJogador=escolheJogador+1;
                 }
             }
@@ -52,34 +52,41 @@ public class Jogo {
             if(escolheJogador==0){
                 System.out.println("\n\n\n\n\nposicao invalida\n");
                 
-            for(int i=0; i<3 ; i++){       
+            for(int i=0; i<3; i++){       
                  
                   for(int j=0 ; j<3 ; j++){
-                        if(posicao = = m[i][j]){
-                             m[i][j]='X';
-                             escolheJogador = escolheJogador + 1;
+                             if(Posicao==m[i][j]){
+                                   m[i][j]='O';
+                                   escolheJogador=escolheJogador+1;
             }
     }        
-            else{
-            System.out.println("jogador '2' , entre com uma posicao");
-            posicao=scanner.nextLine().charAt (0);
-            
-            for(int i=0; i<3; i++){
-            for(int j=0 ; j<3 ; j++){
-            if(posicao==m[i][j]){
-                m[i][j]='0';
-               escolheJogador=escolheJogador+1;
-          
-                    }
-                    }
+            }if(escolheJogador==0){
+                             System.out.println("\n\n\n\n\nPosição Inválida\n");       
+                        }
+                       
+                  }
+               
+            for(int i=0; i<3 ; i++){      
+                 
+                  for(int j=0 ; j<3 ; j++){
+                       
+                        System.out.printf("   "+m[i][j]);
+                  }
+                 
+                  System.out.println("\n");
             }
-                if(escolheJogador==0){
-                System.out.println("\n\n\n\n\nposicao invalida\n");
-            }   
+           
+            if(m[0][0]=='X'&&m[0][1]=='X'&&m[0][2]=='X' || m[1][0]=='X'&&m[1][1]=='X'&&m[1][2]=='X' || m[2][0]=='X'&&m[2][1]=='X'&&m[2][2]=='X' || m[0][0]=='X'&&m[1][0]=='X'&&m[2][0]=='X' || m[0][1]=='X'&&m[1][1]=='X'&&m[2][1]=='X'|| m[0][2]=='X'&&m[1][2]=='X'&&m[2][2]=='X'|| m[0][0]=='X'&&m[1][1]=='X'&&m[2][2]=='X'|| m[2][0]=='X'&&m[1][1]=='X'&&m[0][2]=='X'){
+                  System.out.println("\n\n\n\n\njogador 1 ganhou\n\n\n\n\n\n\n\n");
+                Ganhou=1;
             }
-                 for(int i=0; i<3 ; i++){
-                 for(int j=0 ; j<3 ; j++){
-                     System.out.printf("  "+m[i][j]);
-                 }
-              System.out.println("\n");
-
+                 
+           
+              if(m[0][0]=='O'&&m[0][1]=='O'&&m[0][2]=='O' || m[1][0]=='O'&&m[1][1]=='O'&&m[1][2]=='O' || m[2][0]=='O'&&m[2][1]=='O'&&m[2][2]=='O' || m[0][0]=='O'&&m[1][0]=='O'&&m[2][0]=='O' || m[0][1]=='O'&&m[1][1]=='O'&&m[2][1]=='O'|| m[0][2]=='O'&&m[1][2]=='O'&&m[2][2]=='O'|| m[0][0]=='O'&&m[1][1]=='O'&&m[2][2]=='O'|| m[2][0]=='O'&&m[1][1]=='O'&&m[0][2]=='O'){
+                  System.out.println("\n\n\n\n\njogador 2 ganhou\n\n\n\n\n\n\n\n");
+                  Ganhou=1;
+            }
+           
+            }while(Ganhou!=1);
+            }
+            }
